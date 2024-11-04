7ea6a3f608cd1b8105819650c28e2e774f614479
kirangajjana <kiran.gajjana@gmail.com>
2024-11-04 18:14:07 +0530

    added all properties

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class Placement:
         self.percentage=percentage
         self.college=college
     def Analysis(self):
-        if self.percentage>=90:
+        if self.percentage>=90: #checking wether the  person will get the placement or not based on the percentage
             print("Hi,Congrats you have an capability to select in placement drive based on the below provided data by you")
             print(f"Your gud name is: {self.name}")
             print(f"your branch : {self.branch}")
@@ -23,5 +23,26 @@ class Placement:
             print(f"your city: {self.city}")
             print(f"your percentage : {self.percentage}")      
             print(f"your college : {self.college}") 
+list_placements=[]
+while True:
+    name=input("please enter your name")
+    branch=input("please enter your branch")
+    city=input("please eneter your branch")
+    percentage=float(input("please enter your percentage"))
+    college=input("please enter your college name")
+    k=Placement(name,branch,city,percentage,college)   
+    list_placements.append(k)
+    print("your data has been added successfully")
+    option=input("do you want to add nore data [Yes|NO]")
+    if option.lower()=='no':
+        break
+
+
+print("All the placement data") # we 2will be displaying all the plcement data added here
+print('#'*40)
+for data in list_placements:
+    data.Analysis()
+       
+
     
 
