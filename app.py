52e77da8e39110b631587cc628d8c709a7328921
kirangajjana <kiran.gajjana@gmail.com>
2024-11-04 17:55:57 +0530

    adding constructor

---
+++ app.py
@@ -0,0 +1,6 @@
+class Placement:
+    ''' this class was developed by kiran gajjana'''
+    '''by using this class the user can know wether they will be selected to the placement or not based on percentage'''
+
+    def __init__(self,name):
+        self.name=name
